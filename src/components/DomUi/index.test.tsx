23ad09cb511dc81bfc3452d3ff902d2b974ae5ab
xlcodes <xiaolin201809@126.com>
2023-02-14 18:07:00 +0800

    feat: UI组件库测试用例补充

--- src/components/DomUi/index.test.tsx
+++ src/components/DomUi/index.test.tsx
@@ -4,22 +4,25 @@
 import React from "react";
 import {render, screen} from "@testing-library/react"
 import DomUi from "./index";
+import {act} from "react-dom/test-utils";
 
 beforeEach(() => {
-    render(<DomUi />)
+    render(<DomUi/>)
 })
 
 describe('Ui组件库测试用例', () => {
     it('form validation without semi', () => {
-        const form = screen.getByRole('form', { name: 'form' })
-        const username = screen.getByRole('textbox', { name: 'form_username' })
-        const age = screen.getByRole('spinbutton', { name: "form_age" })
-        const manCheckbox = screen.getByRole('radio', { checked: true, name: "form_sex" })
-        const womanCheckbox = screen.getByRole('radio', { checked: false, name: "form_sex" })
+        const form = screen.getByRole('form', {name: 'form'})
+        const username = screen.getByRole('textbox', {name: 'form_username'})
+        const age = screen.getByRole('spinbutton', {name: "form_age"})
+        const manCheckbox = screen.getByRole('radio', {checked: true, name: "form_sex"})
+        const womanCheckbox = screen.getByRole('radio', {checked: false, name: "form_sex"})
         expect(username).toBeDisabled()
         expect(age).toBeRequired()
         expect(age).toBeEnabled()
-        age.focus()
+        act(() => {
+            age.focus()
+        })
         expect(age).toHaveFocus()
         expect(manCheckbox).toBeChecked()
         expect(womanCheckbox).not.toBeChecked()
@@ -31,6 +34,25 @@ describe('Ui组件库测试用例', () => {
         expect(age).toHaveValue(23)
     })
     it('visible validation with semi', () => {
-
+        // const form = screen.getByRole('form', {name: 'semi-form'})
+        const username = screen.getByLabelText('username')
+        const age = screen.getByLabelText('age')
+        // const sex = screen.getByLabelText('sex')
+        // const hobby = screen.getByLabelText('hobby')
+        expect(username).toBeDisabled()
+        expect(age).toBeEnabled()
+        expect(age).toBeRequired()
+        act(() => {
+            age.focus()
+        })
+        expect(age).toHaveFocus()
+        // expect(username).toHaveValue('zhenmin');
+        // expect(hobby).toHaveValue('code');
+        // expect(form).toHaveFormValues({
+        //   username: 'zhenmin',
+        //   age: 23,
+        //   sex: 'man',
+        //   hobby: 'code',
+        // });
     })
 })
